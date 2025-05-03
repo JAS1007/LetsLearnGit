85c55e9f7eb816587ceb90a0897e1fd67bf5f3ce
JAS1007 <imjasvathsa@gmail.com>
2025-05-03 14:04:39 +0530

    done changes in Diff.java

--- src/git/Diff.java
+++ src/git/Diff.java
@@ -3,8 +3,8 @@ package git;
 public class Diff {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a =20;
+		int b = 10;
+		System.out.println(a-b);
 	}
-
 }
